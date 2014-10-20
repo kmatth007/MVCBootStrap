a3203cc5b5d15be68d42b7e43330682b6e894223
kmatth007 <kmatth007@yahoo.com>
2014-10-20 11:49:44 -0400

    Added  new views and controllers

--- Application.WebUI/Scripts/Contact.js
+++ Application.WebUI/Scripts/Contact.js
@@ -1,1 +1,19 @@
-﻿
+﻿function ProfilesViewModel() {
+    var self = this;
+    self.Profiles = ko.observableArray([
+         {
+             "ProfileId": 1,
+             "FirstName": "Anand",
+             "LastName": "Pandey",
+             "Email": "anand@anandpandey.com"
+         },
+    {
+        "ProfileId": 2,
+        "FirstName": "John",
+        "LastName": "Cena",
+        "Email": "john@cena.com"
+    }
+    ]);    
+}
+
+ko.applyBindings(new ProfilesViewModel());
